d7e9141c550624308fcf7d2bad7460ced9063a5e
evanssamwel <snyamuevans@gmail.com>
2025-04-17 00:21:39 +0300

    more changes

--- backend/src/resources/teacher.py
+++ backend/src/resources/teacher.py
@@ -1,6 +1,7 @@
 from flask_restful import Resource
 from flask import request
-from werkzeug.security import safe_str_cmp
+import hmac
+
 from flask_jwt_extended import (
     create_access_token,
     create_refresh_token
